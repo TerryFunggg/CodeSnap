11434f11485e22b6d8fa1f79e00c8553da7913c7
TerryFunggg <terryyessfung@gmail.com>
2020-01-02 14:56:50 +0800

    update swift

--- Swift/Basic.playground/Contents.swift
+++ Swift/Basic.playground/Contents.swift
@@ -68,6 +68,18 @@ if let name = optionalName {
 let nickName : String? = nil
 let fullName : String = "John Applessed"
 let informanlGreeting = "Hi \(nickName ?? fullName)"
+// switch
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber","watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy? \(x)?")
+default:
+    print("Everything tastes good in soup")
+}
 
 
 
