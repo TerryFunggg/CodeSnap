2e580d761bcf797f82d5b5baa27ab5955f7c1ea2
TerryFunggg <terryyessfung@gmail.com>
2020-01-10 18:11:15 +0800

    add ios

---
+++ IOS/SwiftUI/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  TestSwiftUI
+//
+//  Created by TerryFung on 2020/1/7.
+//  Copyright © 2020 TerryFung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height:300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("Califronia")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
