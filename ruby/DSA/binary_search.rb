cf8118d798c8eefd1da99e1a93028621dc54ffbe
Terry Funggg <41190384+TerryFunggg@users.noreply.github.com>
2021-01-15 00:17:30 +0800

    Update binary_search.rb

--- ruby/DSA/binary_search.rb
+++ ruby/DSA/binary_search.rb
@@ -5,9 +5,10 @@ def binary_search(list,item)
   while low <= high do
     mid = (low + high) / 2
     guess = list[mid]
+    return mid if guess == item
+    
     high = mid - 1 if guess > item
     low = mid + 1 if guess < item
-    return mid if guess == item
   end
 end
 
